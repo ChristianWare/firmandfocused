65f957d7dfefdfb38a2922dec9960102f7de252c
Chris Ware <c.p.ware@hotmail.com>
2024-10-02 15:54:07 -0700

    Solution Section Start

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero/Hero";
 import Nav from "@/components/Nav/Nav";
 import ProblemSection from "@/components/ProblemSection/ProblemSection";
+import SolutionSection from "@/components/SolutionSection/SolutionSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Nav />
       <Hero />
       <ProblemSection />
+      <SolutionSection />
     </main>
   );
 }
---
+++ components/SolutionSection/SolutionSection.tsx
@@ -0,0 +1,10 @@
+import styles from "./SolutionSection.module.css";
+
+const SolutionSection = () => {
+  return (
+    <section className={styles.container}>
+      <h2>Solutions Section</h2>
+    </section>
+  );
+};
+export default SolutionSection;
